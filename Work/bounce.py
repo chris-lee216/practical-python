a1f536004d2fa6a66946a1dbce98060064ee3fea
Chris Lee <chris@noreply.github.com>
2022-04-24 20:31:59 -0400

    Added test case and formatting to the script. Will need to create more complete test cases later.

--- Work/bounce.py
+++ Work/bounce.py
@@ -5,7 +5,7 @@
 # Exercise 1.5
 
 import argparse
-
+import pytest
 
 # -------------------------------------------------------
 def get_args():
@@ -18,11 +18,11 @@ def get_args():
     )
 
     parser.add_argument(
-        "-h",
+        "-H",
         "--height",
         metavar="int",
-        type=int,
-        default=60,
+        type=float,
+        default=60.0,
         help="height of initial drop",
     )
     args = parser.parse_args()
@@ -30,16 +30,29 @@ def get_args():
 
 
 # -------------------------------------------------------
-def bounce(num, height):
+def bounce(height, bounce_back):
     """bounce dat..."""
+    return height * bounce_back
+
+
 # -------------------------------------------------------
+def test_bounce():
+    assert bounce(60.0, 0.6) == 36.0
+    assert bounce(36.0, 0.6) == 21.599999999999998
+
+
+# -------------------------------------------------------
+
+
 def main():
     """Its just begun"""
     args = get_args()
     bounce_back = 0.6  # 3/5
     num_bounces = args.number
-    starting_height = args.height
-
+    height = args.height
+    for i in range(1, num_bounces + 1):
+        print(f"{i:2} {round(height, 4):.4f}")
+        height = bounce(height, bounce_back)
 
 
 # -------------------------------------------------------
