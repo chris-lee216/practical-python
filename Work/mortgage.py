ce01f0563f1c8a79e476501d628ebfa88cd6af28
Chris Lee <chris@noreply.github.com>
2022-04-25 20:34:21 -0400

    More refactoring. This took longer than I would have liked...need more test cases to verify correctness.

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -10,7 +10,8 @@ import argparse
 def get_args():
     """Get command line args"""
     parser = argparse.ArgumentParser(
-        description="Mortgage Calculator", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        description="Mortgage Calculator",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
     parser.add_argument(
@@ -20,22 +21,59 @@ def get_args():
         "-r", "--rate", type=float, help="mortgage rate percentage", default="3.5"
     )
 
+    parser.add_argument("-e", "--extra", type=float, help="extra payment", default=0.0)
+    parser.add_argument(
+        "-s",
+        "--start",
+        type=int,
+        help="extra payment start month",
+        metavar="MONTH",
+        default=1,
+    )
+    parser.add_argument(
+        "-E",
+        "--end",
+        type=int,
+        help="extra payment end month",
+        metavar="MONTH",
+        default=12,
+    )
+
     args = parser.parse_args()
     return args
-    # -------------------------------------------------------
+
+
+# ----------------------------------------------------------------------------------------
+
+
+def calc_payment(principal, rate, payment, extra_payment, start, end):
+    """calculate monthly payments"""
+    total_paid = 0.0
+    month = start
+    while principal > 0:
+        if month < end:
+            principal = principal * (1 + rate / 12) - (payment + extra_payment)
+            total_paid = total_paid + payment + extra_payment
+        else:
+            principal = principal * (1 + rate / 12) - (payment)
+            total_paid = total_paid + payment
+        print(f"{month} ${total_paid:,.2f} ${principal:,.2f}")
+        month += 1
+
+
+# ----------------------------------------------------------------------------------------
 
 
 def main():
     args = get_args()
-    principal = 500000.0
-    rate = 0.05
+    principal = args.principal
+    rate = args.rate / 100
     payment = 2684.11
     total_paid = 0.0
-
-    while principal > 0:
-        principal = principal * (1 + rate / 12) - payment
-        total_paid = total_paid + payment
-    print(f"Total paid: ${total_paid:,.2f}")
+    extra_payment = args.extra
+    start = args.start
+    end = args.end + 1
+    calc_payment(principal, rate, payment, extra_payment, start, end)
 
 
 if __name__ == "__main__":
