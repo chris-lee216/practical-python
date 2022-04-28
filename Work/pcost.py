0bec12df8dee4ce2e69415b87d863f9f0af57860
Chris Lee <chris@noreply.github.com>
2022-04-28 07:39:06 -0400

    Simple program using the csv module.

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,24 @@
 # pcost.py
 #
+# Author: Chris Lee
+# Date: April 28, 2022
 # Exercise 1.27
+
+import csv
+
+
+def main():
+    """It starts here"""
+    path = "Data/portfolio.csv"
+    total_cost = 0
+    with open(path, newline="") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            shares = int(row.get("shares"))
+            price = float(row.get("price"))
+            total_cost += shares * price
+    print(f"Total cost: ${total_cost:,.2f}")
+
+
+if __name__ == "__main__":
+    main()
